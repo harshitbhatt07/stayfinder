0db17a750178d6a665065c657a9a9696eee39ba6
Himanshu <himanshubhatt070707@gmail.com>
2026-06-23 11:33:43 +0530

    fix server startup

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-module.exports = async function connectDB(){
-  try{
-    await mongoose.connect(process.env.MONGODB_URI || process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/stayfinder');
+
+module.exports = async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
     console.log('MongoDB connected');
-  }catch(err){
+  } catch (err) {
     console.error('MongoDB error:', err.message);
     process.exit(1);
   }
-}
+};
